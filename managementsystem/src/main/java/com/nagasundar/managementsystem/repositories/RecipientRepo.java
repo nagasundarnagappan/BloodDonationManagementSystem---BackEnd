2385f512a29130df1fab38dac76cd9972c49e2f2
NAGASUNDAR N <99973079+nagasundarnagappan@users.noreply.github.com>
2023-03-08 14:55:23 +0530

    Delete managementsystem directory

--- managementsystem/src/main/java/com/nagasundar/managementsystem/repositories/RecipientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nagasundar.managementsystem.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nagasundar.managementsystem.models.Recipient;
-
-public interface RecipientRepo extends JpaRepository<Recipient, Integer> {
-    
-}
